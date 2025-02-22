d251d0a3f6194657e576bf080d3103c9c693659a
Sachin Kumar <sachininmindfire@gmail.com>
2025-02-22 20:52:38 +0530

    Added new website for personal blog and added links

--- serve.py
+++ serve.py
@@ -1,23 +1,15 @@
 import http.server
 import socketserver
 
-PORT = 5500
-DIRECTORY = "."
+PORT = 8000
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=DIRECTORY, **kwargs)
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+})
 
-    def end_headers(self):
-        # Add CORS headers
-        self.send_header('Access-Control-Allow-Origin', '*')
-        super().end_headers()
-
-print(f"Serving at http://localhost:{PORT}")
-print("Press Ctrl+C to stop")
-
+print(f"Starting server at http://localhost:{PORT}")
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\nServer stopped")
+    print("Press Ctrl+C to stop the server")
+    httpd.serve_forever()
